82696b6fcbdde9802e63a9ab1af90922f860c9da
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-02-13 15:06:55 -0500

    change name folder context

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../context/Theme";
+import { ThemeContext } from "../Context/ThemeContext";
 
 export default function useTheme() {
   const data = useContext(ThemeContext);
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import ThemeProvider from "../context/Theme";
+import ThemeProvider from "../Context/ThemeContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
